068f17b9785b676d3518db3e9a9adeeeb41ebe9a
Buddhi Rangana <82770183+buddhirangana@users.noreply.github.com>
2022-03-29 09:36:21 +0530

    Add files via upload

---
+++ perl 1.pl
@@ -0,0 +1,13 @@
+print "Hello,\n\n";
+print"Buddhi Rangana\n\n";
+print"You are Good boy buddhi\n\n";
+print "What are you doing men?\n\n";
+use strict;
+use warnings;
+my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) = localtime( time() );
+print "The time is $hour:$min\n\n";
+print "######Design by Buddhi######\n\n";
+
+
+
+
